543e92f68e88a7bd49742904f015f3b2759c627c
Morris CT Shen <32235978+ChunTingShen@users.noreply.github.com>
2023-03-20 15:24:27 -0400

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatButtonModule } from '@angular/material/button';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TopbannerComponent } from './topbanner/topbanner.component';
 import { LowerbannerComponent } from './lowerbanner/lowerbanner.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopbannerComponent,
-    LowerbannerComponent
+    LowerbannerComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
