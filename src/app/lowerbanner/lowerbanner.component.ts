6a15c6747f7572617576c5c13a248b4251de2eb3
Morris CT Shen <32235978+ChunTingShen@users.noreply.github.com>
2023-03-20 15:25:07 -0400

    lower banners

--- src/app/lowerbanner/lowerbanner.component.ts
+++ src/app/lowerbanner/lowerbanner.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-lowerbanner',
   templateUrl: './lowerbanner.component.html',
   styleUrls: ['./lowerbanner.component.css']
 })
 export class LowerbannerComponent {
+  constructor(private router: Router){}
 
+
+  toHome() {
+    console.log("to Home page")
+    this.router.navigate(['/'])
+    
+  }
+
+
+  toContact() {
+    
+    console.log("to Contact page")
+    this.router.navigate(['/contact'])
+    
+  }
 }
