b2fe7b0c66111d6ac945b3720ba8c5da4e0ac7c3
Morris CT Shen <32235978+ChunTingShen@users.noreply.github.com>
2023-03-20 15:24:17 -0400

    Add about component

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent {
+  title = 'Morris Shen';
+  intro = 'Over 6 years of strong software experience designing, developing and deploying web-based business applications using web technologies including frontend framework in Angular 8+, JavaScript (ES6), TypeScript, HTML5, CSS3, SCSS; backend services in Node.js, NestJS; database systems in Postgres and MongoDB.';
+
+
+}
