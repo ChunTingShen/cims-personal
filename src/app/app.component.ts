15a5d292b8a9b811cb2e99514d174a9d03e2f43a
Morris CT Shen <32235978+ChunTingShen@users.noreply.github.com>
2023-03-20 15:24:42 -0400

    modify app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Morris Shen';
-  intro = 'Over 6 years of strong software experience designing, developing and deploying web-based business applications using web technologies including frontend framework in Angular 8+, JavaScript (ES6), TypeScript, HTML5, CSS3, SCSS; backend services in Node.js, NestJS; database systems in Postgres and MongoDB.';
-
 
 
 
