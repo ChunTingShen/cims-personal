43005437268905839910a1717b56784e4384c0c9
Morris CT Shen <32235978+ChunTingShen@users.noreply.github.com>
2023-03-20 15:24:25 -0400

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ContactComponent } from './contact/contact.component';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+  {
+    path: '', redirectTo: '/about', pathMatch: 'full'
+  },
+  {
+    path: 'about', component: AboutComponent
+  },
+  {
+    path: 'contact', component: ContactComponent
+  },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
